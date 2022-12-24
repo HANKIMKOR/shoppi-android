b3026fb53d5cac2d2d85f2944cdc2299208fb8b5
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2022-12-24 16:18:05 +0900

    [섹션7] 과제 전

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -1,14 +1,16 @@
 package com.shoppi.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +25,19 @@ class HomeFragment: Fragment() {
         val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
         button.setOnClickListener {
             findNavController().navigate(R.id.action_home_to_product_detail)
+        }
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
+
+        if (homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+            val titleValue = Title(text, iconUrl)
+            titleValue.text
         }
     }
 }
